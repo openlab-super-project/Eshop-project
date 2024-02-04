0e148510e26e7aba150b1fc6877bac737e554f14
Winlex1 <113421468+Winlex1@users.noreply.github.com>
2024-02-04 22:50:39 +0100

    Added design for products , communication from home page to BE , design for products page and fixed some issues.

--- Eshop-project/ClientApp/src/app/app.module.ts
+++ Eshop-project/ClientApp/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AuthorizeGuard } from 'src/api-authorization/authorize.guard';
 import { AuthorizeInterceptor } from 'src/api-authorization/authorize.interceptor';
 import { ProductsComponent } from './products/products.component';
 import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
--- Eshop-project/ClientApp/src/app/home/home.component.ts
+++ Eshop-project/ClientApp/src/app/home/home.component.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent {
+
+  public productData: HomeProductsDTO[] = [];
+
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string, public router: Router ) {
+    http.get<HomeProductsDTO[]>(baseUrl + 'products').subscribe(result => {
+      this.productData = result;
+    }, error => console.error(error));
+  }
+  shopNow() {
+    this.router.navigate(['/products']);
+  }
+}
+interface HomeProductsDTO {
+  productId: number;
+  productName: string;
+  productDescription: string;
+  price: number;
+  productCategory: string;
+  productImage0: string;
+  productImage1: string;
+  productImage2: string;
 }
