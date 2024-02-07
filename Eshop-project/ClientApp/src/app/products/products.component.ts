f9a2e6b33a832543971cdac38914cc2401e47989
Winlex1 <113421468+Winlex1@users.noreply.github.com>
2024-02-07 21:50:45 +0100

    Added titles for pages,

--- Eshop-project/ClientApp/src/app/products/products.component.ts
+++ Eshop-project/ClientApp/src/app/products/products.component.ts
@@ -28,9 +28,9 @@ export class ProductsComponent {
     this.productData.sort((a, b) => a.price - b.price); // from least expensive
   }
 
-  showAvaiableProducts() {
+  /*showAvaiableProducts() {
     this.ourFilteredProducts = this.productData.filter(product => product.quantity > 0); // musime TOTO OPRAVIT ZAJTRA 
-  }
+  }*/
 
   onSortChange(event: any) {
     const selectedValue = event.target.value;
@@ -39,9 +39,9 @@ export class ProductsComponent {
     } else if (selectedValue === 'leastExpensive') {
       this.sortDataSecond();
     }
-    else if (selectedValue === 'isAvailable') {
+    /*else if (selectedValue === 'isAvailable') {
       this.showAvaiableProducts();
-    }
+    }*/
   }
 }
 
